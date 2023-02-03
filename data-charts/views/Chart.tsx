53a0ce9ee25db559a38cc8f6528b6197144793b6
thomas IMPERY <thomas.impery@gmail.com>
2023-02-03 08:40:46 +0100

    WIP

--- data-charts/views/Chart.tsx
+++ data-charts/views/Chart.tsx
@@ -1,39 +1,72 @@
 import React, { ReactElement } from 'react'
 import { View, StyleSheet } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text } from 'react-native-svg'
 import { PieChart } from 'react-native-svg-charts'
 
 export function Chart(): ReactElement {
-  const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+  const data = [
+    {
+      key: 1,
+      amount: 50,
+      svg: { fill: '#600080' },
+    },
+    {
+      key: 2,
+      amount: 50,
+      svg: { fill: '#9900cc' }
+    },
+    {
+      key: 3,
+      amount: 40,
+      svg: { fill: '#c61aff' }
+    },
+    {
+      key: 4,
+      amount: 95,
+      svg: { fill: '#d966ff' }
+    },
+    {
+      key: 5,
+      amount: 35,
+      svg: { fill: '#ecb3ff' }
+    }
+  ]
 
-  const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
-
-  const pieData = data
-    .filter(value => value > 0)
-    .map((value, index) => ({
-        value,
-        svg: {
-            fill: randomColor(),
-            onPress: () => console.log('press', index),
-        },
-        key: `pie-${index}`,
-    }))
+    const Labels = ({ slices, height, width }) => {
+      return slices.map((slice, index) => {
+        const { labelCentroid, pieCentroid, data } = slice;
+        return (
+          <Text
+            key={index}
+            x={pieCentroid[ 0 ]}
+            y={pieCentroid[ 1 ]}
+            fill={'white'}
+            textAnchor={'middle'}
+            alignmentBaseline={'middle'}
+            fontSize={24}
+            stroke={'black'}
+            strokeWidth={0.2}>
+              {data.amount}
+          </Text>
+        )
+      })
+    }
 
   return (
-    <View style={[
+     <View style={[
         styles.container,
         {
           // Try setting `flexDirection` to `"row"`.
           flexDirection: 'column',
         },
-      ]}>    
-
-        <Text style={{flex:1, textAlign:'center'}}>Ceci est un graphique</Text>
+      ]}>
         <PieChart
-          style={ { flex:10 } }
-          data={ pieData }
-        />
-
+          style={{ height: 200, flex:10 }}
+          valueAccessor={({ item }) => item.amount}
+          data={data}
+          outerRadius={'95%'}>
+            <Labels/>
+        </PieChart>
     </View>
   )
 }
